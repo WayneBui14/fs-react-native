aa7ff05d481ffaafa5e2feddce624f0521c15471
Wayne PC <damienbft@gmail.com>
2026-05-25 12:34:28 +0700

    3. The reviewed repositories list

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native'
+
+const RepositoryItem = ({ props }) => {
+    return (
+        <View>
+            <Text>FullName: {props.fullName}</Text>
+            <Text>Description: {props.description}</Text>
+            <Text>Language: {props.language}</Text>
+            <Text>Stars: {props.stargazersCount}</Text>
+            <Text>Forks: {props.forksCount}</Text>
+            <Text>Reviews: {props.reviewCount}</Text>
+            <Text>Rating: {props.ratingAverage}</Text>
+        </View>
+    )
+}
+
+
+export default RepositoryItem
